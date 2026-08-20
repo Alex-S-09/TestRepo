a5248a73c3e3b5d31368b039a84581251aaa05b4
Glowdisk <infernaldestinymc@gmail.com>
2026-08-19 23:13:24 -0700

    Save changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseAuto.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Autonomous
+public class BaseAuto {
+    private Follower follower;
+
+    private Timer pathTimer, opModeTimer;
+
+    public Enum PathState; {
+
+    }
+
+    PathState pathState;
+
+
+}
